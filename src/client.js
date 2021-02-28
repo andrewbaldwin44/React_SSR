4d0e650a34e402a897e0b308f04db44452a986cc
Andrew Baldwin <andrew.baldwin44@gmail.com>
2021-02-28 18:31:04 -0500

    Fix client side redux

--- src/client.js
+++ src/client.js
@@ -2,13 +2,13 @@ import React from "react";
 import { hydrate } from "react-dom";
 import { Provider } from "react-redux";
 import App from "./index";
-import configureStore from "./redux/reducers";
+import configureStore from "./redux/store";
 
-// const initialState = window.__INITIAL_STATE__;
-//
-// delete window.__INITIAL_STATE__;
+const initialState = window.__INITIAL_STATE__;
 
-const store = configureStore();
+delete window.__INITIAL_STATE__;
+
+const store = configureStore(initialState);
 
 hydrate(
   <Provider store={store}>
